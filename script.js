30279ad12f513a565632faf60fa9cf84bd89419d
divyasreenivas <n170860@rguktn.ac.in>
2023-08-09 21:50:36 +0530

    project done by dhivvya

---
+++ script.js
@@ -0,0 +1,39 @@
+let firstnumber = 3
+let secondnumber = 9
+let sum= firstnumber+secondnumber
+let blackjack = false 
+let alive =true
+let message =""
+let messageel=document.getElementById("message-el")
+let sumel = document.getElementById("sum-el")
+let cardel = document.getElementById("card-el")
+
+function startgame(){
+    rendergame()
+}
+function rendergame(){  
+    cardel.textContent="cards:"+firstnumber +" "+ secondnumber
+    sumel.textContent = "sum :"+ sum    
+    if(sum<21){
+        message="sad face "
+    }
+    else if (sum===21){
+        message="happy face"
+        blackjack = true
+    }
+    else {
+        message="cry face"
+        alive=false
+    }
+    messageel.textContent=message
+}
+function newcard(){
+    console.log("draw a card from deck ")
+    let cardnumber=6
+    sum +=cardnumber
+    rendergame()
+
+}
+
+
+
